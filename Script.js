deabd7404e5e405ef0b8fd52c25ae625bb8e162b
Eniyanoto Alade <alad0023@algonquinlive.com>
2024-05-21 10:43:15 -0400

    Commit at may 2024.

--- Script.js
+++ Script.js
@@ -1,10 +1,26 @@
+//to make the search bar work
 filterObjects("all");
 
+function search_bar(){
+    let input = document.getElementById('search-bar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('filterDiv');
+
+    for(i = 0; i < x.length; i++){
+        if(!x[i].innerHTML.toLowerCase().includes(input)){
+            x[i].style.display="none";
+        }
+        else{
+            x[i].style.display="block";
+        }
+    }
+}
+//filter
 function filterObjects(c){
     var x, i;
     x = document.getElementsByClassName("filterDiv");
     if (c == "all") c = "";
-    for (i = 0; 1 < x.length; i++){
+    for (i = 0; i < x.length; i++){
         removeClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
     }
@@ -16,7 +32,7 @@ function addClass(element, name){
     arr2 = name.split(" ");
     for (i = 0; i < arr2.length; i++){
         if (arr1.indexOf(arr2[i]) == -1){
-            element.className += " " +arr2[i];
+            element.className += " " + arr2[i];
         }
     }
 }
